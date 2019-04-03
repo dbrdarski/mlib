6d1afaa6fac72689644a9c6ffaaf2404b772f34c
Dane Brdarski <dane.brdarski@gmail.com>
2019-04-03 20:52:43 +0200

    removed redundant code

--- src/State/index.js
+++ src/State/index.js
@@ -1,5 +1,3 @@
-// FIX: Nested objects not immutable/frozen!!!!!
-
 // TODO: Freeze on Create State...
 // TODO: Investigate: deep freeze, deep assign.
 
@@ -22,66 +20,28 @@ const CreateState = window.CreateState = (state = {}, options) => {
 		);
 	};
 	const observer = CreateProxy(state, { handler, mutable });
-
 	return {
 		state: observer,
 		subscribe,
 		unsubscribe
 	};
-}
-
-
-// const mutatorTarget = { current: null };
-// const fn = (prop, len) => (...args) => {
-// 	!(args.length < len){
-// 		mutatorTarget.current()
-// 	}
-// }
-
-
+};
 
 const StateGuard = (state, { mutable = false } = {}) => {
 	let dirty = false;
 	return () => {
 		if( mutable && dirty ){
-			console.log('RETURNING STATE', { mutable, dirty })
 			return state;
 		}
-		console.log('COPYING STATE')
 		dirty = mutable;
 		return state = copy(state);
 	}
-}
-
-const mutatorList = {
-	pop: 0,
-	shift: 0,
-	push: 1,
-	unshift: 1,
-	splice: 0,
-	reverse: 0,
-	fill: 0,
-	sort: 0
-}
-
-// ArrayMutatorProto = Object.define({}, );
-
-// class ObservableArray extends Array {
-// 	constructor(...props){
-// 		super(...props);
-// 	}
-// 	push(...args){
-// 		if (args.length){
-// 			Array.prototype.push.call(this);
-// 		}
-// 	}
-// }
-
-const apply = (fn) => fn();
-const applyToObjectKeys = (proxy) => (v, k) => {
-	return isPrimitive(v) || proxy[k]();
 };
 
+const mutatorList = { pop: 0, shift: 0, push: 1, unshift: 1, splice: 0, reverse: 0, fill: 0, sort: 0 };
+
+const apply = (fn) => fn();
+const applyToObjectKeys = (proxy) => (v, k) => isPrimitive(v) || proxy[k]();
 
 
 const SubProxy = (subarray, prop, subproxies, { handler, mutable }) => {
@@ -92,10 +52,9 @@ const SubProxy = (subarray, prop, subproxies, { handler, mutable }) => {
 		});
 	}
 	return subproxies[prop];
-}
+};
 
-var CreateProxy = window.CreateProxy = (record, { handler, mutable = false }) => {
-
+const CreateProxy = window.CreateProxy = (record, { handler, mutable = false }) => {
 	let proxy,
 			subproxies = {},
 			state = StateGuard(record, { mutable });
@@ -111,9 +70,7 @@ var CreateProxy = window.CreateProxy = (record, { handler, mutable = false }) =>
 	const mutators = isArray && mutable
 		? Object.defineProperties({}, map(mutatorList, defineMutatorFn))
 		: null;
-
 	return proxy = new Proxy(new Function, {
-
 		get: (target, prop, parent) => {
 			if (record.hasOwnProperty(prop)) {
 				const p = record[prop];
@@ -130,7 +87,6 @@ var CreateProxy = window.CreateProxy = (record, { handler, mutable = false }) =>
 			}
 		},
 		set: (target, prop, value) => {
-			console.log("SET", { value, prop, record })
 			if(!record.hasOwnProperty(prop) || record[prop] !== value){
 				record = state();
 				record[prop] = value;
@@ -139,7 +95,6 @@ var CreateProxy = window.CreateProxy = (record, { handler, mutable = false }) =>
 				handler && handler(record);
 			}
 		},
-
 		deleteProperty: (target, prop) => {
 			if( record.hasOwnProperty(prop) ){
 				record = state();
@@ -149,7 +104,6 @@ var CreateProxy = window.CreateProxy = (record, { handler, mutable = false }) =>
 				handler && handler(record);
 			}
 		},
-
 		apply: ( target, thisArg, args ) => {
 			if(!args.length){
 				Object.freeze(record);
@@ -171,236 +125,9 @@ var CreateProxy = window.CreateProxy = (record, { handler, mutable = false }) =>
 			}
 		}
 	});
-}
-
-// const ImutableArray = (array, handler) => {
-// 	let state = StateGuard(array, { mutable: false });
-// 	const proxy = new Proxy(new Function, {
-// 		get: (target, prop, parent) => {
-// 			if (array.hasOwnProperty(prop)) {
-// 				const p = array[prop];
-// 				return isPrimitive(p)
-// 					? p
-// 					: CreateProxy(p, { mutable: true })
-// 			} else if (Array.prototype.hasOwnProperty(prop)) {
-// 				return Array.prototype.bind(array);
-// 			}
-// 		},
-// 		set: (target, prop, value) => {
-// 			if(!array.hasOwnProperty(prop) || array[prop] !== value) {
-// 				array = state();
-// 				array[prop] === value;
-// 			}
-// 		},
-// 		apply: ( target, thisArg, args ) => {
-// 			Object.freeze(array);
-// 			state = StateGuard(array);
-// 			handler && handler(array);
-// 			return array;
-// 		}
-// 	})
-// }
-//
-// const MutableArray = (array, handler) => {
-// 	let state = StateGuard(array, { mutable: true });
-// 	const defineMutatorFn = (len, pop) => {
-// 		value: (...args) => {
-// 			if ( args.length >= len ) {
-// 				array = state();
-// 			}
-// 			return Array.prototype[prop].appy(array, args);
-// 		}
-// 	};
-// 	const mutators = Object.define({}, map(mutatorList, defineMutatorFn));
-// 	const proxy = new Proxy(new Function, {
-// 		get: (target, prop, parent) => {
-// 			if (array.hasOwnProperty(prop)) {
-// 				const p = array[prop];
-// 				return isPrimitive(p)
-// 					? p
-// 					: CreateProxy(p, { mutable: true })
-// 			} else if (mutators.hasOwnProperty(prop)) {
-// 				return mutators[prop];
-// 			} else if (Array.prototype.hasOwnProperty(prop)){
-// 				return Array.prototype.bind(array);
-// 			}
-// 		},
-// 		set: (target, prop, value) => {
-// 			if(!array.hasOwnProperty(prop) || array[prop] !== value){
-// 				array = state();
-// 				array[prop] === value;
-// 			}
-// 		},
-// 		apply: ( target, thisArg, args ) => {
-// 			Object.freeze(array);
-// 			state = StateGuard(array);
-// 			handler && handler(array);
-// 			return array;
-// 		}
-// 	});
-// }
-//
-// const spawnChildProxy = (childrenProxies, prop, p, parent, mutable, log) => {
-// 	if (childrenProxies[prop] == null) {
-// 		childrenProxies[prop] = CreateProxy(p, {
-// 			parent,
-// 			property: prop,
-// 			mutable,
-// 			log
-// 		});
-// 	}
-// 	return childrenProxies[prop];
-// }
-
-// const StateFactory = (state, {parent, property, mutable} = {}) => {
-// 	let dirty = false;
-// 	return ({ stateUpdate, mutate, freeze, log }) => {
-// 		state = (!stateUpdate || (mutate ? dirty : mutable && mutable !== dirty)) ? state : copy(state);
-// 		console.log('++FACTORY++')
-// 		console.log({ stateUpdate, mutate, dirty, mutable, cond: [!stateUpdate, mutate, dirty, mutable] })
-// 		console.log('+++++++++++')
-// 		if (isCallable(stateUpdate)) {
-// 			stateUpdate(state);
-// 		} else if (stateUpdate) {
-// 			state = stateUpdate;
-// 		}
-// 		if ( !mutate ) {
-// 			if ( stateUpdate && !dirty && parent != null ) parent[property] = state;
-// 		}
-// 		dirty = mutate;
-// 		freeze && Object.freeze(state);
-// 		return state;
-// 	};
-// }
-
-// const mutators = {
-// 	push: true
-// };
-
-// const CreateProxy = () => false; // (record, { parent, property, handler, mutable, log } = {}) => {
-// 	let proxy,
-// 			isArray = Array.isArray(record),
-// 			childrenProxies = {},
-//
-// 			state = StateGuard(record, { mutable });
-//
-// 	return proxy = new Proxy(new Function, {
-// 		ownKeys: (target) => Reflect.ownKeys(target),
-// 		apply: ( target, thisArg, args ) => {
-// 			// console.log('==APPLY==')
-// 			// console.log({ property, target, args, parent, isArray, record });
-// 			// console.log("=======")
-//
-// 			// console.log({ property, record, isCallable: isCallable(record), apply: {target, thisArg, args} })
-// 			// console.log('===APPLY===')
-// 			if(isCallable(record)){
-// 				console.log('Simple call!!!!!!!!!!!!');
-// 				return record.apply(thisArg, args);
-// 			}
-// 		  const [ stateUpdate, options ] = args;
-// 			if( !args.length ){
-// 				record = state({
-// 					stateUpdate: mutable
-// 						? (record) => each(record, applyToObjectKeys(proxy))
-// 						: false,
-// 					mutate: false,
-// 					freeze: true,
-// 					// log: true
-// 				});
-// 				handler && handler(record);
-// 				return record;
-// 			} else {
-// 				// mutable = true;
-// 				const subProxy = CreateProxy(record, {
-// 					mutable: true,
-// 					log: true,
-// 					handler: proxyResult => console.log({proxyResult})
-// 				});
-// 				if (isPrimitive(stateUpdate)) {
-// 					throw new Error(ERR_STATE_UPDATE)
-// 				} else if (isCallable(stateUpdate)) {
-// 					stateUpdate(subProxy);
-// 				} else {
-// 					Object.assign(subProxy, stateUpdate);
-// 				}
-// 				// mutable = false;
-// 				record = state({
-// 					stateUpdate: subProxy(),
-// 					mutate: false,
-// 					freeze: true,
-// 					// log: true
-// 				});
-// 				return record;
-// 			}
-// 		},
-// 		get: (target, prop, parent) => {
-// 			console.log("==GET==")
-// 			console.log({ property, target, prop, parent, isArray, record });
-// 			console.log("=======")
-// 			let p = record[prop];
-// 			if(isArray && mutators[prop] != null && p === record.constructor.prototype[prop]){
-// 				console.log('=====IS==ARRAYISH=====')
-// 				return (...args) => {
-// 					// console.log({p, record})
-// 					record = state({
-// 						stateUpdate: (record) => {
-// 							p.apply(record, args);
-// 							// proxy();
-// 						},
-// 						mutate: mutable,
-// 						log: true
-// 					});
-// 				};
-// 			}
-// 			return isPrimitive(p)
-// 				? p
-// 				// : isCallable(p)
-// 				// 	? p.bind(proxy) // maybe it should be binded to Proxy instead
-// 					: spawnChildProxy(childrenProxies, prop, p, parent, mutable);
-// 		},
-// 		set: (target, prop, value) => {
-// 			// console.log('==SET==')
-// 			// console.log({ property, target, prop, value, isArray, record });
-// 			// console.log("=======")
-//
-// 			// console.log({ target, prop, value })
-// 			// console.log('SET')
-// 			const condition = state[prop] !== value;
-// 			if( condition ){
-// 				record = state({
-// 					stateUpdate: (record) => record[prop] = value,
-// 					mutate: mutable
-// 				});
-// 				delete childrenProxies[prop];
-// 				// should childrenProxies allways be deleted (for deleteProperty)?
-// 				mutable || handler && handler(record);
-// 			}
-// 			return true;
-// 		},
-// 		deleteProperty: (target, prop) => {
-// 			const condition = record.hasOwnProperty(prop);
-// 			if( condition ){
-// 				record = state({
-// 					stateUpdate: (record) => delete record[prop],
-// 					mutate: mutable
-// 				});
-// 				delete childrenProxies[prop];
-// 				// should childrenProxies allways be deleted (for deleteProperty)?
-// 				mutable || handler && handler(record);
-// 			}
-// 		}
-// 	});
-// }
+};
 
 module.exports = {
 	CreateState,
 	CreateProxy
 }
-// state.search.term = '';
-// s1 = state();
-// state.search.term = 'asd'
-// s2 = state();
-// s3 = state((DRAFT) => {
-// 	state.items.push(1,2,3);
-// });
-// s2 === s3
